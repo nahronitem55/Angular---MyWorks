630a99449399c7cfacdd4eaa64accd7d9bccdebe
orhan.metin <orhan.metin@etiya.com>
2021-09-08 13:41:34 +0300

    Category
    *ngFor ile kategorlier listelendi

--- Angular-Ex-Shop/shop/src/app/category/category.component.ts
+++ Angular-Ex-Shop/shop/src/app/category/category.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from './category';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
+  constructor() {}
+  title = "Kategori Listesi"
+  categories: Category[] = [
+    { id: 1, categoryName: 'Elektronik' }, 
+    { id: 2, categoryName: 'Bilgisayar' },
+    { id: 3, categoryName: 'Telefon' },
+    { id: 4, categoryName: 'İletişim' }, 
+    { id: 5, categoryName: 'Müzik' }                      
+];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
