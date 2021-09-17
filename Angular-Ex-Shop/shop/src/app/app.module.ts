1fcc949bc5e78fef0e483c20ca704c1519e42900
orhan.metin <orhan.metin@etiya.com>
2021-09-17 10:17:06 +0300

    Forms worked

--- Angular-Ex-Shop/shop/src/app/app.module.ts
+++ Angular-Ex-Shop/shop/src/app/app.module.ts
@@ -8,7 +8,8 @@ import { CategoryComponent } from './category/category.component';
 import { ProductComponent } from './product/product.component';
 import { ProductFilterPipe } from './product/product-filter.pipe';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { ProductAddForms1Component } from './product/product-add-forms1/product-add-forms1.component'
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HttpClientModule } from '@angular/common/http'
     NavComponent,
     CategoryComponent,
     ProductComponent,
-    ProductFilterPipe
+    ProductFilterPipe,
+    ProductAddForms1Component
   ],
   imports: [
     BrowserModule,
